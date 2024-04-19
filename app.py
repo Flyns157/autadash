8fabe3873598f1a7dee6d46589cdff141a45afc5
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-04-19 19:51:59 +0200

    ajout d'une page de test

--- app.py
+++ app.py
@@ -93,6 +93,10 @@ def logout():
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 # =================================== Run specifications ===================================
 if __name__ == '__main__':
     app.run(debug=True)
