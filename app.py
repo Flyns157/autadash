57c67c35f408059ff16fd61e19f662d4ab2bcd27
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-04-23 07:48:10 +0200

    Add email input to signup form, validate email format and uniqueness, display error messages
    The changes include:
    
    * Adding an email input field to the signup form
    * Validating the email format using a regular expression

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import re
+from random import random, choice
+import time
+from datetime import datetime
+import string
 # personnal modules
 import debug_sys
+
+# =================================== Utils ===================================
+def generate_key(size: int = 50) -> str:
+    """
+    Generate a random key
+    """
+    return ''.join(str(datetime.now()).split()) + ''.join(choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(size))
 
 # =================================== Init Flask app ===================================
 app = Flask(__name__, static_url_path='',
@@ -20,6 +32,7 @@ login_manager.init_app(app)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
 
     def set_password(self, password):
@@ -43,20 +56,34 @@ def load_user(user_id):
 def signup():
     if request.method == 'POST':
         username = request.form.get('username')
+        email = request.form.get('email')
         password = request.form.get('password')
+
+        # Vérifier le format de l'email
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            flash('Format d\'email invalide')
+            return redirect(url_for('signup'))
+
+        # Vérifier si l'email est jetable
+        if email.split('@')[1] in ['jetable.com', 'yopmail.com']:
+            flash('Veuillez utiliser une adresse email non jetable')
+            return redirect(url_for('signup'))
+        
+        # Vérifier si l'email existe déjà
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('L\'email est déjà utilisé')
+            return redirect(url_for('signup'))
 
         user = User.query.filter_by(username=username).first()
         if user:
             flash('Le nom d\'utilisateur existe déjà')
-            debug_sys.log('INFO', f'Attempt to creat a new user ! >> username = {username}, password = {password}')
             return redirect(url_for('signup'))
 
-        new_user = User(username=username)
+        new_user = User(username=username, email=email)
         new_user.set_password(password)
         db.session.add(new_user)
         db.session.commit()
-
-        debug_sys.log('INFO', f'New user created ! >> {new_user}')
 
         return redirect(url_for('login'))
     return render_template('auth/signup.html')
@@ -64,10 +91,10 @@ def signup():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form.get('username')
+        username_or_email = request.form.get('username_or_email')
         password = request.form.get('password')
 
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter((User.email == username_or_email) | (User.username == username_or_email)).first()
         if not user or not user.check_password(password):
             flash('Veuillez vérifier vos identifiants')
             return redirect(url_for('login'))
