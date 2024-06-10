ed2b842d59808167cd32ffa69eba88306ffa3b97
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-06-10 10:51:22 +0200

    update requirement.txt and run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import autadash
 # =================================== Main ===================================
 def main():
     autadash.init()
-    autadash.app.run(debug=True)
+    autadash.app.run(debug=True, host='0.0.0.0', port=5000)
 
 # =================================== Run ===================================
 if __name__ == '__main__':
